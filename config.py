928c6a0faa3af8c994be8da5e38e81d31daf4f39
JuliaElenaSC <julia.elena.sc@gmail.com>
2023-12-01 16:54:06 +0100

    conessione al database

--- config.py
+++ config.py
@@ -1,50 +1,20 @@
-#configurazione nel server # Patron DAO (Data Acces Object, per mantenere la persistenza dei dati)
-from flask import Flask
-from bson import ObjectId
 from pymongo import MongoClient
+from flask import Flask
+from flask_pymongo import PyMongo
+import pymongo
 
-app = Flask(__name__) 
-client= MongoClient('mongodb+srv://BIGDATA1:BIGDATA1@bigdata1.atmuofo.mongodb.net/')
-db=client.proveITSAR
+app = Flask(__name__)
 
-def openClose():
-    client = MongoClient('mongodb+srv://BIGDATA1:BIGDATA1@bigdata1.atmuofo.mongodb.net/')
+myClient= pymongo.MongoClient('mongodb+srv://BIGDATA1:BIGDATA1@bigdata1.atmuofo.mongodb.net/')
+mydb=myClient['proveITSAR']
+myCollection=mydb['catasto_geojson']
 
-    db = client["does-not-exist"]
-    print(db)
-
-    db = client.proveITSAR
-    print(db)
-
-    db = client["proveITSAR"]
-    print(db)
-
-    comments = db.comments
-    print(comments)
-
-    comments = db["comments"]
-    print(comments)
-
-    client.close()
-
-def find():
-    client = MongoClient('mongodb+srv://BIGDATA1:BIGDATA1@bigdata1.atmuofo.mongodb.net/')
-
-    query = {
-        "nome": "Giuseppe" 
-    }
-    projection = {
-        
-        "_id": 0
-    }
-    
-    cursor = client.proveITSAR.catasto_ssg.find(filter=query,limit=1)
-
-    for post in cursor:
-        print(post)
-       
-    client.close()
-
-if __name__ == '__main__':
-    
-    find()
+cod_fisc = input("Inserisci il codice fiscale: ")
+resultado = myCollection.find_one({"properties.cod_fisc": cod_fisc})
+if resultado:
+    print("Clase:", resultado['properties']['fclass'])
+    print("name:", resultado['properties']['name'])
+    print("Nome:", resultado['properties']['nome'])
+    print("Cognome:", resultado['properties']['cognome'])
+else:
+    print("No se encontraron datos para el código fiscal proporcionado.")
