978195e68d3cbcc8849dec316ca5dd185cedaf1c
Jose Luis Lopez Michme <j.lopezmichme@itsrizzoli.it>
2023-12-02 16:24:55 +0100

    Aggiunta la possibilita di modificare le misure della mappa.

--- static/main.js
+++ static/main.js
@@ -1,5 +1,15 @@
 var map = L.map('map').setView([45.4642, 9.19], 13);
 
+// Imposta le dimensioni della mappa
+function setMapSize(height, width) {
+    map.invalidateSize(); // Aggiorna la dimensione della mappa
+    map.getContainer().style.height = height + 'px';
+    map.getContainer().style.width = width + 'px';
+}
+
+//impostare a piacere la misura per la mappa. se da problemi commentare dallaa riga 4 fino alla riga 11
+setMapSize(500, 800);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
